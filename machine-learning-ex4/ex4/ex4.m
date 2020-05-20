cfdc9fe28cc3ad75118b3cf87497495110cba862
aviralgoel <aviralgoel@rocketmail.com>
2020-05-20 09:41:43 +0400

    cost function with regularization
    added computation function for cost of nueral network with paramater regularisation

--- machine-learning-ex4/ex4/ex4.m
+++ machine-learning-ex4/ex4/ex4.m
@@ -88,19 +88,19 @@ pause;
 %  continue to implement the regularization with the cost.
 %
 
-%%%fprintf('\nChecking Cost Function (w/ Regularization) ... \n')
+fprintf('\nChecking Cost Function (w/ Regularization) ... \n')
 
 % Weight regularization parameter (we set this to 1 here).
-%%%lambda = 1;
+lambda = 1;
 
-%%%J = nnCostFunction(nn_params, input_layer_size, hidden_layer_size, ...
-                   %%%num_labels, X, y, lambda);
+J = nnCostFunction(nn_params, input_layer_size, hidden_layer_size, ...
+                   num_labels, X, y, lambda);
 
-%%%fprintf(['Cost at parameters (loaded from ex4weights): %f '...
-         %%%'\n(this value should be about 0.383770)\n'], J);
+fprintf(['Cost at parameters (loaded from ex4weights): %f '...
+         '\n(this value should be about 0.383770)\n'], J);
 
-%%%fprintf('Program paused. Press enter to continue.\n');
-%%%pause;
+fprintf('Program paused. Press enter to continue.\n');
+pause;
 
 
 %% ================ Part 5: Sigmoid Gradient  ================
