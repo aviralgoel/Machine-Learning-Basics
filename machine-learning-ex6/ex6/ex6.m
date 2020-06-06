4df28e32012e869b62a8d2845f846fbdd021b82a
aviralgoel <aviralgoel@rocketmail.com>
2020-06-07 03:27:17 +0400

    created spam email classifer
    using svm

--- machine-learning-ex6/ex6/ex6.m
+++ machine-learning-ex6/ex6/ex6.m
@@ -136,15 +136,16 @@ pause;
 
 % Load from ex6data3: 
 % You will have X, y in your environment
-%%%load('ex6data3.mat');
+load('ex6data3.mat');
 
 % Try different SVM Parameters here
-%%%[C, sigma] = dataset3Params(X, y, Xval, yval);
-
+[C, sigma] = dataset3Params(X, y, Xval, yval);
+C
+sigma
 % Train the SVM
-%%%model= svmTrain(X, y, C, @(x1, x2) gaussianKernel(x1, x2, sigma));
-%%%visualizeBoundary(X, y, model);
+model= svmTrain(X, y, C, @(x1, x2) gaussianKernel(x1, x2, sigma));
+visualizeBoundary(X, y, model);
 
-%%%fprintf('Program paused. Press enter to continue.\n');
-%%%pause;
+fprintf('Program paused. Press enter to continue.\n');
+pause;
 
