e99e0025f97765d141fba21268c479b271b80e09
aviralgoel <aviralgoel@rocketmail.com>
2020-06-23 01:28:19 +0400

    pca algorithm implemented

--- machine-learning-ex7/ex7/ex7_pca.m
+++ machine-learning-ex7/ex7/ex7_pca.m
@@ -43,29 +43,30 @@ pause;
 %  You should now implement PCA, a dimension reduction technique. You
 %  should complete the code in pca.m
 %
-%%%fprintf('\nRunning PCA on example dataset.\n\n');
+fprintf('\nRunning PCA on example dataset.\n\n');
 
 %  Before running PCA, it is important to first normalize X
-%%%[X_norm, mu, sigma] = featureNormalize(X);
+[X_norm, mu, sigma] = featureNormalize(X);
 
 %  Run PCA
-%%%[U, S] = pca(X_norm);
+[U, S] = pca(X_norm);
+
 
 %  Compute mu, the mean of the each feature
 
 %  Draw the eigenvectors centered at mean of data. These lines show the
 %  directions of maximum variations in the dataset.
-%%%hold on;
-%%%drawLine(mu, mu + 1.5 * S(1,1) * U(:,1)', '-k', 'LineWidth', 2);
-%%%drawLine(mu, mu + 1.5 * S(2,2) * U(:,2)', '-k', 'LineWidth', 2);
-%%%hold off;
+hold on;
+drawLine(mu, mu + 1.5 * S(1,1) * U(:,1)', '-k', 'LineWidth', 2);
+drawLine(mu, mu + 1.5 * S(2,2) * U(:,2)', '-k', 'LineWidth', 2);
+hold off;
 
-%%%fprintf('Top eigenvector: \n');
-%%%fprintf(' U(:,1) = %f %f \n', U(1,1), U(2,1));
-%%%fprintf('\n(you should expect to see -0.707107 -0.707107)\n');
+fprintf('Top eigenvector: \n');
+fprintf(' U(:,1) = %f %f \n', U(1,1), U(2,1));
+fprintf('\n(you should expect to see -0.707107 -0.707107)\n');
 
-%%%fprintf('Program paused. Press enter to continue.\n');
-%%%pause;
+fprintf('Program paused. Press enter to continue.\n');
+pause;
 
 
 %% =================== Part 3: Dimension Reduction ===================
